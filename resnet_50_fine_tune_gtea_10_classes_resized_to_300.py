fabea7c33f50ef27ddf2dce3383994c038210c93
Sagar Verma <sagar15056@iiitd.ac.in>
2017-12-26 12:09:33 +0530

    first commit

---
+++ resnet_50_fine_tune_gtea_10_classes_resized_to_300.py
@@ -0,0 +1,245 @@
+
+
+from __future__ import print_function, division
+import shutil
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+
+def make_weights_for_balanced_classes(images, nclasses):                        
+    count = [0] * nclasses                                                      
+    for item in images:                                                         
+        count[item[1]] += 1                                                     
+    weight_per_class = [0.] * nclasses                                      
+    N = float(sum(count))                                                   
+    for i in range(nclasses):                                                   
+        weight_per_class[i] = N/float(count[i])                                 
+    weight = [0] * len(images)                                              
+    for idx, val in enumerate(images):                                          
+        weight[idx] = weight_per_class[val[1]]                                  
+    return weight
+
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomSizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Scale(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
+
+data_dir = '/home/shubham/Egocentric/dataset/GTea_preprocessed_rgb_10_actions_resized_300'
+
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+print (image_datasets)
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=128,
+                                             shuffle=True, num_workers=4)
+#print (dataloaders)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+use_gpu = torch.cuda.is_available()
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=2000):
+    since = time.time()
+
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
+    ##------my code------------##
+    train_loss = []
+    train_acc = []
+    test_acc = []
+    test_loss = []
+    val_loss = 100
+    ##--------finish--------------##
+    for epoch in range(num_epochs):
+        
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for data in dataloaders[phase]:
+                # get the inputs
+                
+                inputs, labels = data
+                # wrap them in Variable
+                if use_gpu:
+                    inputs = Variable(inputs.cuda())
+                    labels = Variable(labels.cuda())
+                else:
+                    inputs, labels = Variable(inputs), Variable(labels)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                outputs = model(inputs)
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.data[0]
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+
+            #print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            #    phase, epoch_loss, epoch_acc))
+            
+            ##----------my code --------------##
+            if phase=='train':
+                train_loss.append(epoch_loss)
+                train_acc.append(epoch_acc)
+                print ('##############################################################')
+                print ("{} loss = {}, acc = {},".format(phase, epoch_loss, epoch_acc))                
+            else:
+                test_loss.append(epoch_loss)
+                test_acc.append(epoch_acc)
+                print (" {} loss = {}, acc = {},".format(phase, epoch_loss, epoch_acc))
+                print ('##############################################################')
+
+       
+            plt.plot(range(len(train_loss)),train_loss,label="Train")
+            plt.plot(range(len(test_loss)),test_loss,label="Test")
+            plt.legend(bbox_to_anchor=(.90, 1), loc=2, borderaxespad=0.)
+            plt.savefig(file_name+"_loss.png")
+            plt.cla()
+            plt.clf()
+            plt.plot(range(len(train_acc)),train_acc,label="Train")
+            plt.plot(range(len(test_acc)),test_acc,label="Test")
+            plt.legend(bbox_to_anchor=(.90, 1), loc=2, borderaxespad=0.)
+            plt.savefig(file_name+"_acc.png")
+            plt.cla()
+            plt.clf()
+
+
+            ##--------- my code finish--------------##
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                #best_model_wts = model.state_dict()
+                torch.save(model, 'weights_'+file_name+'_lr_001.pt')
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    #model.load_state_dict(best_model_wts)
+    model = torch.load('weights_'+file_name+'_lr_001.pt')
+    return model
+
+
+######################################################################
+# Visualizing the model predictions
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+#
+# Generic function to display predictions for a few images
+#
+
+def visualize_model(model, num_images=6):
+    images_so_far = 0
+    fig = plt.figure()
+
+    for i, data in enumerate(dataloaders['val']):
+        inputs, labels = data
+        if use_gpu:
+            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+        else:
+            inputs, labels = Variable(inputs), Variable(labels)
+
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+
+        for j in range(inputs.size()[0]):
+            images_so_far += 1
+            ax = plt.subplot(num_images//2, 2, images_so_far)
+            ax.axis('off')
+            ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+            imshow(inputs.cpu().data[j])
+
+            if images_so_far == num_images:
+                return
+
+
+
+if __name__ == '__main__':
+
+    file_name = __file__.split('/')[-1].split('.')[0]
+    print (file_name)
+    model_conv = torchvision.models.resnet50(pretrained=True)
+    for param in model_conv.parameters():
+        param.requires_grad = False
+
+    # Parameters of newly constructed modules have requires_grad=True by default
+    num_ftrs = model_conv.fc.in_features
+    model_conv.fc = nn.Linear(num_ftrs, 10)
+    
+    
+    if use_gpu:
+        model_conv = model_conv.cuda()
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that only parameters of final layer are being optimized as
+    # opoosed to before.
+    optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+    #optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.000001)
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=.1)
+    #exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv)
+
+
+    ######################################################################
+    # Train and evaluate
+    # ^^^^^^^^^^^^^^^^^^
+    #
+    # On CPU this will take about half the time compared to previous scenario.
+    # This is expected as gradients don't need to be computed for most of the
+    # network. However, forward does need to be computed.
+    #
+
+    model_conv = train_model(model_conv, criterion, optimizer_conv,
+                             exp_lr_scheduler, num_epochs=500)
+
+    ######################################################################
+    #
+    visualize_model(model_conv)
+    plt.ioff()
+    plt.show()
